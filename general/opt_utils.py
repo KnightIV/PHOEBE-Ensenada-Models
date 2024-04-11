9aa5fcc8b63c1d1f04d9880f415d2088d6ad7b61
KnightIV <ramon.caballeroiv@gmail.com>
2024-04-11 16:28:46 -0600

    Initial commit Initial PHOEBE models for 4 contact systems Same Jupyter notebook copied to each system, acts on different data Dubious models highlighted in notebook names Initial model estimates; next step is parameter optimization

---
+++ general/opt_utils.py
@@ -0,0 +1,77 @@
+import os
+from collections import namedtuple
+
+import phoebe
+
+import general.utils as gen_utils
+
+AdoptSolutionResult = namedtuple("AdoptSolutionResult", "solutionName computeModelName")
+def adopt_solution(b: phoebe.Bundle, label:str=None, 
+					reset_params=False, solution_file:str=None, adopt_twigs:list[str]=None,
+					run_compute=True, print_sol=True, compute='phoebe01', **compute_kwargs) -> AdoptSolutionResult:
+	solutionName: str
+	if label is not None:
+		solutionName = f"opt_{label}_solution"
+
+	if solution_file:
+		solutionName = b.import_solution(solution_file, overwrite=True).solutions[0]
+		label = solutionName.replace("_solution", "").replace("opt_", "")
+
+	if print_sol:
+		print("Adopted:")
+		gen_utils.printFittedVals(b, solutionName, adopt_twigs=adopt_twigs)
+		print("\nOriginal values:")
+		gen_utils.printFittedTwigsConstraints(b, solutionName, adopt_twigs=adopt_twigs)
+
+	try:
+		initValues = {}
+		if reset_params:
+			for twig in b.get_value(qualifier='fitted_twigs', solution=solutionName):
+				initValues[twig] = b.get_quantity(twig)
+
+		b.adopt_solution(solutionName, adopt_parameters=adopt_twigs)
+
+		computeModelName = None
+		if run_compute: 
+			computeModelName = f"opt_{label}_model"
+			b.run_compute(model=computeModelName, compute=compute, **compute_kwargs, overwrite=True)
+	except: # reset values if an exception occurs, regardless of reset_params value
+		for twig, val in initValues.items():
+			b.set_value(twig, value=val)
+	finally:
+		if reset_params:
+			for twig, val in initValues.items():
+				b.set_value(twig, value=val)
+	
+	return AdoptSolutionResult(solutionName, computeModelName)
+
+def optimize_params(b: phoebe.Bundle, fit_twigs: list[str], label: str, export: bool, datasets: list[str], subfolder: str=None, 
+					optimizer='optimizer.nelder_mead', compute='phoebe01', overwrite_export=True,
+					**solver_kwargs):
+	if not 'maxiter' in solver_kwargs.keys():
+		solver_kwargs['maxiter'] = 200 if export else 10
+
+	abilitatedDatasets = [d for d in b.datasets if b.get_value(qualifier='enabled', dataset=d)]
+	gen_utils.abilitateDatasets(b, datasets, False)
+	
+	saveIterProgress = 1 if export and optimizer == 'optimizer.nelder_mead' else 0
+	b.add_solver(optimizer, solver=f'opt_{label}', fit_parameters=fit_twigs, overwrite=True, progress_every_niters=saveIterProgress, compute=compute, **solver_kwargs)
+	if export:
+		if not os.path.exists('external-jobs'):
+			os.mkdir('external-jobs')
+		if subfolder is not None:
+			os.makedirs(os.path.join('external-jobs', subfolder), exist_ok=True)
+		
+		exportPath = f'./external-jobs{f"/{subfolder}" if subfolder is not None else ""}/{optimizer}_opt_{label}.py'
+		if not overwrite_export and os.path.exists(exportPath):
+			print("Solver already exists |", exportPath)
+		else:
+			fname, out_fname = b.export_solver(script_fname=exportPath, out_fname=f'./results/opt_{label}_solution', 
+												solver=f'opt_{label}', solution=f'opt_{label}_solution', overwrite=True)
+			print("External Solver:", fname, out_fname)
+	else:
+		b.run_solver(solver=f'opt_{label}', solution=f'opt_{label}_solution', overwrite=True, **solver_kwargs)
+
+	gen_utils.abilitateDatasets(b, abilitatedDatasets)
+	
+	return f'opt_{label}', f'opt_{label}_solution'
---
+++ general/utils.py
@@ -0,0 +1,274 @@
+import os
+
+import phoebe
+from phoebe import u
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+from matplotlib.animation import FuncAnimation
+from IPython import display
+import ipywidgets
+
+SPM_COLORS = {
+    'lcSpmJohnsonB@dataset': 'b',
+	'Johnson:B': 'b',
+    'lcSpmJohnsonB@model': 'darkblue',
+
+    'lcSpmJohnsonV@dataset': 'g',
+	'Johnson:V': 'g',
+    'lcSpmJohnsonV@model': 'darkgreen',
+
+    'lcSpmCousinsR@dataset': 'r',
+    'Cousins:R': 'r',
+    'lcSpmCousinsR@model': 'darkred',
+
+    'lcSpmCousinsI@dataset': 'orange',
+    'Cousins:I': 'orange',
+    'lcSpmCousinsI@model': 'goldenrod'
+}
+
+TESS_COLORS = {
+	'lcTess@dataset': 'lightblue',
+	'lcTess@model': 'blue'
+}
+
+ZTF_COLORS = {
+	'lcZtfR@dataset': 'r',
+	'lcZtfR@model': 'darkred',
+
+	'lcZtfG@dataset': 'g',
+	'lcZtfG@model': 'darkgreen',
+
+	'lcZtfI@dataset': 'orange',
+	'lcZtfI@model': 'goldenrod',
+
+	# trimmed datasets
+	'lcZtfRTrimmed@dataset': 'r',
+	'lcZtfRTrimmed@model': 'darkred',
+
+	'lcZtfGTrimmed@dataset': 'g',
+	'lcZtfGTrimmed@model': 'darkgreen',
+
+	'lcZtfITrimmed@dataset': 'orange',
+	'lcZtfITrimmed@model': 'goldenrod'
+}
+
+def displayAnims(rows: int, cols: int, *anims: FuncAnimation):
+	plt.rcParams["animation.html"] = "html5"
+	plt.rcParams["figure.figsize"] = (15,8)
+	
+	grid = ipywidgets.GridspecLayout(n_rows=rows, n_columns=cols)
+	for row in range(rows):
+		for col in range(cols):
+			index = (row*cols) + col
+			anim = anims[index]
+			grid[row, col] = ipywidgets.HTML(anim.to_html5_video())
+
+	display.display(grid)
+
+def displayAnim(anim: FuncAnimation):
+	originalBackend = plt.rcParams['backend']
+	plt.rcParams['backend'] = 'Agg'
+	display.display(display.HTML(anim.to_html5_video()))
+	plt.rcParams['backend'] = originalBackend
+
+def __matchAnyTwig(twig: str, twigs_list: list[str]) -> bool:
+	for refTwig in twigs_list:
+		refComponents = refTwig.split('@')
+		twigComponents = twig.split('@')
+
+		if len(set(refComponents) & set(twigComponents)) != 0:
+			return True
+		
+	return False
+
+def printFittedVals(b: phoebe.Bundle, solution: str, adopt_twigs: list[str] = None, units: dict[str, u.Unit] = {}):
+	for twig, value, unit in zip(b.get_value('fitted_twigs', solution=solution),
+								b.get_value('fitted_values', solution=solution),
+								b.get_value('fitted_units', solution=solution)):
+		try:
+			originalUnit = u.Unit(unit)
+			quantity = value * originalUnit
+			print(twig, f"{quantity.to(units.get(twig, originalUnit)).value:.5f}", units.get(twig, originalUnit).to_string(), "(Not adopting)" if adopt_twigs is not None and not __matchAnyTwig(twig, adopt_twigs) else "")
+		except:
+			print(twig, value, unit)
+
+def printFittedTwigsConstraints(b: phoebe.Bundle, solution: str, units: dict[str, u.Unit] = {}, adopt_twigs: list[str] = None):
+	for fitTwig in b.get_value('fitted_twigs', solution=solution):
+		quantity = b.get_quantity(fitTwig)
+		print("C" if b[fitTwig].constrained_by else " ", 
+				fitTwig, 
+				f"{quantity.to(units.get(fitTwig, quantity.unit)):.5f}", 
+				"(Not adopting)" if adopt_twigs is not None and not __matchAnyTwig(fitTwig, adopt_twigs) else "")
+
+def saveBundle(b: phoebe.Bundle, bundleName: str, subfolder: str = None, overwrite: bool = True) -> str:
+	if not os.path.exists("bundle-saves"):
+		os.mkdir("bundle-saves")
+
+	saveFolder = "bundle-saves"
+	if subfolder:
+		saveFolder = f"bundle-saves/{subfolder}"
+		os.makedirs(saveFolder, exist_ok=True)
+
+	if os.path.exists(os.path.join(saveFolder, bundleName)):
+		if overwrite:
+			print(f"CAUTION: overwriting {os.path.join(saveFolder, bundleName)}")
+		else:
+			print(f"NOT OVERWRITING: {os.path.join(saveFolder, bundleName)} bundle already exists.")
+	
+	return b.save(f"{saveFolder}/{bundleName}")
+
+def genAnimatedMesh(b: phoebe.Bundle, logger=None, meshDataset="mesh01", fc='teffs', **plot_kwargs):
+	if logger: logger.setLevel('ERROR')
+	default_kwargs = {
+		"draw_sidebars": True,
+		"color": "inferno"
+	}
+
+	_, mplfig = b.plot(dataset=meshDataset, kind='mesh', fc=fc, ec='face', animate=True, **(default_kwargs | plot_kwargs))
+	if logger: logger.setLevel('WARNING')
+	return mplfig
+
+def animateMesh(b: phoebe.Bundle, logger=None, meshDataset="mesh01", fc='teffs', **plot_kwargs):
+	displayAnim(genAnimatedMesh(b, logger, meshDataset, fc, **plot_kwargs))
+
+def getEnabledDatasets(b: phoebe.Bundle):
+	enabledDatasets = []
+	for c in b.computes:
+		for d in b.datasets:
+			if b.get_value(qualifier='enabled', compute=c, dataset=d) and d not in enabledDatasets:
+				enabledDatasets.append(d)
+	return enabledDatasets
+
+def abilitateDatasets(b: phoebe.Bundle, enableDatasets: list[str], includeMesh: bool = True):
+	"""
+	Enables specified datasets and disables all others.
+	"""
+	enableDatasets = enableDatasets.copy()
+	if includeMesh:
+		enableDatasets.append('mesh01')
+		
+	for d in b.datasets:
+		if d in enableDatasets:
+			b.enable_dataset(d)
+		else:
+			b.disable_dataset(d)
+		# b.set_value_all(qualifier='enabled', dataset=d, value=(d in localDatasets))
+
+def plotFigSize(b: phoebe.Bundle, figsize: tuple[float, float], **plot_kwargs):
+	fig = plt.figure(figsize=figsize)
+	b.plot(fig=fig, **plot_kwargs)
+
+def plotModelResidualsFigsize(b: phoebe.Bundle, figsize: tuple[float, float], datasetGroups: list[list[str] | str], model: str, 
+							  model_kwargs: dict['str', 'str'] = {}, residuals_kwargs: dict['str', 'str'] = {}, **plot_kwargs) -> dict[str, Figure]:
+	"""
+	Plots specified model for the datasets given. Plots dataset(s) with model overlay alongside residuals side-by-side.
+	"""
+	defaultPlotKwargs = {
+		'marker': {'dataset': '.'},
+		'color': SPM_COLORS | TESS_COLORS | ZTF_COLORS,
+		'legend': True,
+		'ls': {'model': 'solid'}
+	}
+	for key, defaultVal in defaultPlotKwargs.items():
+		plot_kwargs[key] = plot_kwargs.get(key, defaultVal)
+
+	residuals_kwargs['marker'] = '.'
+
+	datasetGroupsFigures = {}
+	for datasets in datasetGroups:
+		fig = plt.figure(figsize=figsize)
+		b.plot(x='phase', model=model, dataset=datasets, axorder=1, fig=fig, s={'dataset':0.008}, **(plot_kwargs | model_kwargs))
+		b.plot(x='phase', y='residuals', model=model, dataset=datasets, axorder=2, fig=fig, subplot_grid=(1,2), s=0.008, show=True, **(plot_kwargs | residuals_kwargs))
+		datasetGroupsFigures["-".join(datasets)] = fig
+	return datasetGroupsFigures
+
+def exportCompute(b: phoebe.Bundle, model: str, datasets: list[str], subfolder: str = None, **compute_kwargs) -> None:
+	if not os.path.exists("external-compute"):
+		os.mkdir("external-compute")
+
+	computeFolder = "external-compute"
+	if subfolder:
+		computeFolder = f"external-compute/{subfolder}"
+		os.makedirs(computeFolder, exist_ok=True)
+
+	b.export_compute(script_fname=os.path.join(computeFolder, f"{model}.py"), out_fname=os.path.join(computeFolder, "results", f"{model}.model"), 
+				  model=model, dataset=datasets, **compute_kwargs)
+	
+def adopt_solution(b: phoebe.Bundle, solution_name:str=None, model_name: str = None,
+					reset_params=False, solution_file:str=None, 
+					run_compute=True, print_sol=True, print_units={}, compute='phoebe01', **compute_kwargs) -> None:
+	if solution_file:
+		solution_name = b.import_solution(solution_file, solution=solution_name, overwrite=True).solutions[0]
+
+	if print_sol:
+		print("Adopted:")
+		printFittedVals(b, solution_name, units=print_units)
+		print("\nOriginal values:")
+		printFittedTwigsConstraints(b, solution_name, units=print_units)
+
+	try:
+		initValues = {}
+		if reset_params:
+			for twig in b.get_value(qualifier='fitted_twigs', solution=solution_name):
+				initValues[twig] = b.get_quantity(twig)
+
+		b.adopt_solution(solution_name)
+
+		if run_compute: 
+			b.run_compute(model=model_name, compute=compute, **compute_kwargs, overwrite=True)
+	except:
+		if reset_params:
+			for twig, val in initValues.items():
+				b.set_value(twig, value=val)
+	finally:
+		if reset_params:
+			for twig, val in initValues.items():
+				b.set_value(twig, value=val)
+
+
+def printChi2(b: phoebe.Bundle, model: str):
+	"""
+	Prints the chi2 fit of a model for all available datasets: Iturbide, Aviles, Gaia, and ZTF data, both normalized
+	and raw datasets. Silently ignores any dataset that isn't present in the specified model.
+	"""
+
+	# rawGaiaDatasets = [d for d in b.datasets if 'raw' in d and 'gaia' in d]
+	# normGaiaDatasets = [d for d in b.datasets if 'norm' in d and 'gaia' in d]
+	ztfDatasets = [d for d in b.datasets if 'Ztf' in d]
+	spmDatasets = [d for d in b.datasets if 'Spm' in d]
+	
+	print(model, "=================================================", sep='\n')
+
+	# try:
+	# 	print('\t', "Gaia (Raw) -", np.sum(b.calculate_chi2(model=model, dataset=rawGaiaDatasets)))
+	# 	for gd in rawGaiaDatasets:
+	# 		print('\t\t', gd, "-", np.sum(b.calculate_chi2(model=model, dataset=gd)))
+	# except: pass
+	
+	print("------------------------------------------------")
+
+	try:
+		print('\t', "TESS -", np.sum(b.calculate_chi2(model=model, dataset='lcTess')))
+	except: pass
+
+	try:
+		print('\t', "OAN SPM -", np.sum(b.calculate_chi2(model=model, dataset=spmDatasets)))
+		for d in spmDatasets:
+			print('\t\t', d, "-", np.sum(b.calculate_chi2(model=model, dataset=d)))
+	except: pass
+
+	print("------------------------------------------------")
+
+	print('\t', "ZTF -", np.sum(b.calculate_chi2(model=model, dataset=ztfDatasets)))
+	for zd in ztfDatasets:
+		try:
+			print('\t\t', zd, "-", np.sum(b.calculate_chi2(model=model, dataset=zd)))
+		except: 
+			print("\t\t", zd, "Not found in model")
+
+def printAllModelsChi2(b: phoebe.Bundle):
+	for m in b.models:
+		printChi2(b, m)
